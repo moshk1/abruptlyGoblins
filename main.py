4ac26e16e0b8fa39e58527db8845d9e8c6d008be
Anders Morken <andersmorken@gmail.com>
2022-04-07 14:42:57 +0200

    Find available day

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 
-gamers = {}
+gamers = []
 def add_gamer(gamer, gamers_list):
     if gamer.get("name") and gamer.get("availability"):
-        gamers_list.update(gamer)
+        gamers_list.append(gamer)
     else:
         print("Gamer missing critical information")
 
-kimberly = {"name": "Kimberly", "availability": ["Mondays", "Tuesdays", "Fridays"]}
-add_gamer(kimberly, gamers)
-#print((gamers))
+# kimberly = {"name": "Kimberly", "availability": ["Monday", "Tuesday", "Friday"]}
+# add_gamer(kimberly, gamers)
+# print((gamers))
 
 add_gamer({'name':'Thomas Nelson','availability': ["Tuesday", "Thursday", "Saturday"]}, gamers)
 add_gamer({'name':'Joyce Sellers','availability': ["Monday", "Wednesday", "Friday", "Saturday"]}, gamers)
@@ -20,8 +20,7 @@ add_gamer({'name':'Crystal Brewer','availability': ["Thursday", "Friday", "Satur
 add_gamer({'name':'James Barnes Jr.','availability': ["Tuesday", "Wednesday", "Thursday", "Sunday"]}, gamers)
 add_gamer({'name':'Michel Trujillo','availability': ["Monday", "Tuesday", "Wednesday"]}, gamers)
 
-# for key, value in gamers.items():
-#     print(key, ' : ', value)
+# print(*gamers, sep='\n')
 
 def build_daily_frequency_table():
     return {
@@ -37,9 +36,25 @@ count_availability = build_daily_frequency_table()
 
 def calculate_availability(gamers_list, available_frequency):
     for gamer in gamers_list:
-        for day in gamers["availability"]:
+        for day in gamer["availability"]:
             available_frequency[day] += 1
 
 calculate_availability(gamers, count_availability)
-print(count_availability)
+# print(count_availability)
 
+def find_best_night(availability_table):
+    best_availability = 0
+    for day, availability in availability_table.items():
+        if availability > best_availability:
+            best_night = day
+            best_availability = availability
+    return best_night
+
+game_night = find_best_night(count_availability)
+# print(game_night)
+
+def available_on_night(gamers_list, day):
+    return [gamer for gamer in gamers_list if day in gamer['availability']]
+
+attending_game_night = available_on_night(gamers, game_night)
+# print(*attending_game_night, sep='\n')
